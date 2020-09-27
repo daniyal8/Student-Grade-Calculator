33674a73aed727be211ddd9cf975b0e562f24505
daniyal8 <daniyalsadiq4@gmail.com>
2020-09-27 06:41:59 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const add = () => {
+    let eng = document.getElementById('input1').value;
+    let comp = document.getElementById('input2').value;
+    let chemistry = document.getElementById('input3').value;
+    let maths = document.getElementById('input4').value;
+    let grade = ""
+    
+    
+    let totalMarks = parseFloat(eng) + parseFloat(comp) + parseFloat(chemistry) + parseFloat(maths);
+    let percentage = (totalMarks/400) * 100;
+
+    if(percentage <= 100 && percentage >= 80){
+        grade = 'A'
+    }else if(percentage <= 79 && percentage >= 60) {
+        grade = 'B'
+    }else if(percentage <= 59 && percentage >= 40) {
+            grade = 'C'
+    }else{
+        grade = 'F'
+    }   
+
+   if(percentage >= 40){
+    document.getElementById('showData').innerHTML =
+    `Your Total Marks Is ${totalMarks} Out Of 400. And Your Percentage is ${percentage}%. And Your Grade is ${grade}. You Are Pass `  
+   }else{
+    document.getElementById('showData').innerHTML = 
+   `Your Total Marks Is ${totalMarks} Out Of 400. And Your Percentage is ${percentage}%. And Your Grade is ${grade}. You Are fail `  
+   }
+};
+
+
+
